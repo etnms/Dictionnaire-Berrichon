4455e0bd4664774d1be1734cb23ead303ca96c4e
etnms <etiennenemes@gmail.com>
2023-07-01 04:22:05 -0400

    improved some UI elements

--- components/WordDay.tsx
+++ components/WordDay.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from "./WordDay.module.scss";
 import { IWord } from '../utils/types';
 import Spinner from './Spinner';
+import WordDefinition from './WordDefinition';
+import translatePOS from '../utils/translatePOS';
 
 interface IWordOfTheDayProps {
     wordOfTheDay: IWord | null | undefined;
@@ -10,11 +12,13 @@ interface IWordOfTheDayProps {
 function WordDay(props: IWordOfTheDayProps) {
     const { wordOfTheDay } = props;
 
+
     return <div className={styles.container}>
         <h2>Mot du jour</h2>
         {wordOfTheDay === null || wordOfTheDay === undefined ? <Spinner height="60px" width="60px" border="4px solid #f3f3f3" borderTop="4px solid #607196" /> :
+
             <div className={styles["word-container"]}>
-                <p className={styles.word}>{wordOfTheDay?.word} {wordOfTheDay?.pos ? `(${wordOfTheDay?.pos})` : ""}</p>
+                <p className={styles.word}>{wordOfTheDay?.word} {wordOfTheDay?.pos ? `(${translatePOS(wordOfTheDay?.pos)})` : ""}</p>
                 <p>Traduction: {wordOfTheDay?.translation}</p>
                 <p>{wordOfTheDay?.definition ? `Définition: ${wordOfTheDay?.definition}` : null}</p>
             </div>
