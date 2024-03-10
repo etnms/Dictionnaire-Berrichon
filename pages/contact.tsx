1c3aa36f39c4c8a79f0e2d7c1ffd98edb74d9976
etnms <etiennenemes@gmail.com>
2024-03-10 20:54:27 +0100

    added frontend styling and html for contact page and form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import ContactForm from "../components/ContactForm";
 
 const Contact: React.FC = () => {
   return (
     <>
       <Navbar />
+      <main className="bg-white dark:bg-gray-900 min-h-svh">
+        <section className="px-16 mb-12">
+          <h1 className="p-2 text-3xl font-bold leading-none tracking-tight text-gray-900 dark:text-white">
+            Contact
+          </h1>
+          <p className="p-2 dark:text-white">
+            Si vous avez des questions ou si vous voulez suggérer des
+            corrections, ou des mots manquants au dictionnaire, vous pouvez le
+            faire avec le formulaire ci-dessous ou en envoyant un courriel à
+            l'adresse suivante:
+          </p>
+          <a
+            href="mailto: contact@dictionnaire-berrichon"
+            className="p-2 text-purple-600"
+          >
+            contact@dictionnaire-berrichon
+          </a>
+        </section>
+        <ContactForm />
+      </main>
     </>
   );
 };
