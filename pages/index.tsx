a65b2d7363e9148aff2a89066009487445998bbf
etnms <etiennenemes@gmail.com>
2024-04-19 19:22:28 +0200

    created _document for custom stylesheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,10 +54,6 @@ const Home: NextPage = () => {
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <Navbar />
       <main className="py-10 text-center flex flex-col gap-12 justify-center items-center ">
