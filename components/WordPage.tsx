42f63c5eefac8955ecc5ffcfecfb0508f3043628
etnms <etiennenemes@gmail.com>
2024-10-13 18:37:47 -0500

    moved fetch similar word to next frontend

--- components/WordPage.tsx
+++ components/WordPage.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WordSeeAlso from "./WordSeeAlso";
 import Link from "next/link";
 import { SimilarWord, Entry } from "../utils/types";
 import WordDefinition from "./WordDefinition";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
+import Spinner from "./Spinner";
 
 type WordPage = {
   entries: any;
-  similarWords: SimilarWord[];
   languageDirection: string;
 };
 
-const WordPage: React.FC<WordPage> = ({
-  entries,
-  similarWords,
-  languageDirection,
-}) => {
+const WordPage: React.FC<WordPage> = ({ entries, languageDirection }) => {
+  // Fetch similar word from the frontend to lighten backend load
+  const [similarWords, setSimilarWords] = useState<SimilarWord[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    console.log(isLoading);
+    const fetchSimilarWords = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API}/api/similarWords?word=${
+            entries.words[0]?.word || ""
+          }&lang=berrichon-francais`
+        );
+        const data = await response.json();
+        setSimilarWords(data); // Set the fetched similar words to state
+      } catch (error) {
+        //console.error("Failed to fetch similar words:", error);
+      } finally {
+        setIsLoading(false); // Stop loading state once data is fetched
+      }
+    };
+
+    fetchSimilarWords();
+  }, [entries.words]);
+
   return (
     <>
       <Navbar />
@@ -28,7 +49,11 @@ const WordPage: React.FC<WordPage> = ({
               <p className="mb-3 p-2">
                 Aucun résultat n&apos;a été trouvé pour ce mot.
               </p>
-              {similarWords.length === 0 ? null : (
+              {isLoading ? (
+                <p>
+                  Chargement <Spinner />
+                </p>
+              ) : similarWords.length === 0 ? null : (
                 <div className="flex flex-col items-center w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                   <p className="text-xl mb-2 dark:text-white">
                     Autres suggestions:
@@ -65,7 +90,11 @@ const WordPage: React.FC<WordPage> = ({
             </ul>
           )}
         </div>
-        {entries.words.length === 0 ? null : (
+        {isLoading ? (
+          <p>
+            <Spinner />
+          </p>
+        ) : entries.words.length === 0 ? null : (
           <WordSeeAlso
             similarWords={similarWords}
             languageDirection={languageDirection}
--- pages/berrichon-francais/[word].tsx
+++ pages/berrichon-francais/[word].tsx
@@ -4,14 +4,8 @@ import { GetServerSidePropsContext } from "next";
 import WordPage from "../../components/WordPage";
 
 export default function Word(props: EntryResults) {
-  const { entries, similarWords } = props;
-  return (
-    <WordPage
-      entries={entries}
-      similarWords={similarWords}
-      languageDirection="berrichon-francais"
-    />
-  );
+  const { entries } = props;
+  return <WordPage entries={entries} languageDirection="berrichon-francais" />;
 }
 
 // Get props from server side rendering
@@ -19,17 +13,12 @@ export async function getServerSideProps(params: GetServerSidePropsContext) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API}/api/berrichon-francais/${params.query.word}` as string
   );
-  // Always check for similar words, even for empty results, to suggest to user
-  const similarWordsRes = await fetch(
-    `${process.env.NEXT_PUBLIC_API}/api/similarWords?word=${params.query.word}&lang=berrichon-francais` as string
-  );
-  const similarWords = await similarWordsRes.json();
+
   // If empty result (404 on api), send empty entries but similar words
   if (res.status === 404) {
     return {
       props: {
         entries: { words: [] },
-        similarWords,
       },
     };
     // Otherwise get the entries and return props
@@ -37,7 +26,7 @@ export async function getServerSideProps(params: GetServerSidePropsContext) {
     const entries = await res.json();
     // return props
     return {
-      props: { entries, similarWords },
+      props: { entries },
     };
   }
 }
